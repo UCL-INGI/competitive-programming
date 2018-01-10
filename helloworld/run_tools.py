cfe3ac61563f5756417ac590584bb00733ee1f9e
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-01-10 10:22:03 +0100

    Automatic synchronization from INGInious

---
+++ helloworld/run_tools.py
@@ -0,0 +1,154 @@
+import time
+import os
+
+"""
+Checks whether the given file is empty.
+"""
+def is_empty_file(fn):
+  return os.path.getsize(fn) == 0
+
+"""
+Creates a list containing all lines from a given file.
+"""
+def readlines(fn):
+  f = open(fn, 'r')
+  lines = [line.strip() for line in f.readlines()]
+  return lines
+
+"""
+Given a list of strings, create a string containing
+all those strings separated by newlines for INGInious
+presentation.
+"""
+def format_for_output(lines):
+  '\n\n'.join(lines)
+
+"""
+Compiles a java code and returns two values:
+
+- A flag that is true iff the compilation was successful
+- A list will the lines from the compilation error message.
+  The list is empty iff the compilation was sucessful.
+"""
+def compile_java(mainclass):
+  os.system('> err')
+  os.system('javac {0}.java 2> err'.format(mainclass))
+  return is_empty_file('err'), readlines('err')
+
+"""
+Runs a java code on a given input and writes the output in the given
+output file.
+
+Returns the cpu run-time of that code.
+"""
+def run_java(mainclass = 'Main', inputfile = 'input', outputfile = 'output'):
+  os.system('> err')
+  start_time = time.clock()
+  os.system('cat {0} | java {1} > {2} 2> err'.format(inputfile, mainclass, outputfile))
+  end_time = time.clock()
+  run_time = end_time - start_time
+  return is_empty_file('err'), run_time
+
+def judge_java(mainclass = 'Main', testdir = './tests', checker, timelimit):
+  judging = Judging()
+  # compile the student solution
+  compile_ok, err = compile_java(mainclass)
+  if not compile_ok:
+    # compile error
+    judging.set_compile_error()
+    return judging
+  # no compile error, so we run
+  test_index = -1
+  for fn in os.listdir(testdir):
+    if fn.endswith('.in'):
+      test_index += 1
+      # get the name of the test case
+      name = fn.split('.')[-1]
+      run_ok, time = run_java(mainclass, testdir + '/' + fn)
+      # set the runtime
+      judging.add_time(time)
+      if time > timelimit:
+        # time limit exceeded
+        judging.add_time_limit_exceeded(test_index)
+      if not run_ok:
+        # runtime error
+        judging.add_runtime_error(test_index)
+      else:
+        # check whether the answer is correct
+        answer_ok = checker(testdir + '/' + fn, testdir + '/' + name + '.ans', 'output')
+        if not answer_ok:
+          # wrong_answer
+          judging.add_wrong_anser(test_index)
+        else:
+          # correct
+          judging.add_correct(test_index)
+  return judging
+
+class Judging:
+
+  def __init__(self):
+    self.compile_error = False
+    self.wrong_answer = set()
+    self.time_limit_exceeded = set()
+    self.runtime_error = set()
+    self.correct = set()
+    self.run_time = { }
+  
+  def set_compile_error(compile_error = True):
+    self.compile_error = compile_error
+  
+  def add_wrong_anser(test_index):
+    self.wrong_answer.append(test_index)
+  
+  def add_time_limit_exceeded(test_index):
+    self.time_limit_exceeded.append(test_index)
+
+  def add_runtime_error(test_index):
+    self.runtime_error.append(test_index)
+
+  def add_correct(test_index):
+    self.correct.append(test_index)
+
+  def add_time(test_index, run_time):
+    self.run_time[test_index] = run_time
+
+  def is_accepted():
+    return not self.is_compile_error() and not self.is_wrong_answer() and not self.is_time_limit_exceeded() and not self.is_runtime_error()
+  
+  def is_wrong_answer():
+    return len(self.wrong_answer) > 0
+
+  def is_time_limit_exceeded():
+    return len(self.time_limit_exceeded) > 0
+
+  def is_runtime_error():
+    return len(self.runtime_error) > 0
+
+  def is_compile_error():
+    return self.compile_error
+
+  def produce_feedback_message():
+    if self.is_compile_error():
+      return 'Compile error\n\n'.format()
+    s = ''
+    for test_index in self.run_time:
+      status = ''
+      if test_index in self.correct:
+        status = '[CORRECT]'
+      if test_index in self.wrong_answer:
+        status += '[WA]'
+      if test_index in self.time_limit_exceeded:
+        status += '[TLE]'
+      if test_index in self.runtime_error:
+        status += '[RE]'
+      s += 'Test #{0}: runtime={1}, status={2}\n\n'.format(test_index, self.run_time[test_index], status)
+    overall_status = ''
+    if self.is_accepted():
+      overall_status = '[ACCEPTED]'
+    if self.is_wrong_answer():
+      overall_status += '[WRONG ANSWER]'
+    if self.is_time_limit_exceeded():
+      overall_status += '[TIME LIMIT EXCEEDED]'
+    if self.is_runtime_error():
+      overall_status += '[RUNTIME ERROR]'
+    return s
--- helloworld/student/run.py
+++ helloworld/student/run.py
@@ -1,140 +1,26 @@
 import os
 from inginious import feedback
-import checker
+import checkers
 import time
 import math
+from run_tools import *
 
 """
 CONFIG
 """
-verbose = True
 classname = 'Main'
 taskname = 'file'
 solname = 'yunoacsol'
+timelimit = 2
+checker = checkers.diff_check
 """
 END OF CONFIG
 """
 
-def is_empty_file(fn):
-  return os.path.getsize(fn) == 0
-
-def readfile(filename):
-  f = open(filename, 'r')
-  lines = [line.strip() for line in f.readlines()]
-  return '\n'.join(lines)
-
-def compile_java(classname):
-  os.system('> err')
-  os.system('javac {0}.java 2> err'.format(classname))
-  if not is_empty_file('err'):
-    return False, readfile(err)
-  return True, ''
-
-
-def run_tests_java():
-  student_times = [ ]
-  student_output_verdict = [ ]
-  student_output_feedback = [ ]
-  my_times = [ ]
-  # loop over the input files
-  for fn in os.listdir('./tests/'):
-    if fn.endswith('.in'):
-      # input file found
-      if verbose: print('running: {0}'.format(fn))
-      # get the test case name
-      name = fn.split('.')[0]
-      # get the cpu start time for the student solution
-      start_time = time.clock()
-      # run student java solution
-      if verbose: print('running student solution')
-      os.system('cat ./tests/{0} | java {1} > output'.format(fn, classname))
-      # cumpute the runtime
-      student_cpu_time = time.clock() - start_time
-      if verbose: print('student solution finished in: {0}s'.format(student_cpu_time))
-      student_times.append(student_cpu_time)
-      # get the cpu start time for the my solution
-      start_time = time.clock()
-      # run my java solution
-      if verbose: print('running my solution')
-      os.system('cat ./tests/{0} | java {1}'.format(fn, solname))
-      my_cpu_time = time.clock() - start_time
-      if verbose: print('my solution finished in: {0}s'.format(my_cpu_time))
-      my_times.append(my_cpu_time)
-      # update the maxtime
-      ok, message = checker.diff_check('./tests/{0}.ans'.format(name), 'output')
-      # add verdict
-      if ok:
-        student_output_verdict.append(True)
-        student_output_feedback.append('correct')
-      else:
-        student_output_verdict.append(False)
-        student_output_feedback.append(message)
-  return student_times, my_times, student_output_verdict, student_output_feedback
-
-def tle_check(student_time, my_time):
-  if student_time < 1:
-    return False
-  return student_time < 10 * my_time
-
-def output_feedback(student_times, my_times, verdict, output_feedback):
-  s = ''
-  WA_CNT = 0
-  TLE_CNT = 0
-  n = len(student_times)
-  for i in range(n):
-    TLE = False
-    WA = False
-    s += 'test {0}: runtime={1:.2f} '.format(i, student_times[i])
-    if tle_check(student_times[i], my_times[i]):
-      TLE = True
-    if not verdict[i]:
-      WA = True
-    if WA and TLE:
-      WA_CNT += 1
-      TLE_CNT += 1
-      s += '[WA] [TLE]\n\n'
-    elif WA:
-        WA_CNT += 1
-        s += '[WA]\n\n'
-    elif TLE:
-        TLE_CNT += 1
-        s += '[TLE]\n\n'
-    else:
-        s += '[OK]\n\n'
-  if WA_CNT + TLE_CNT == 0:
-    s += 'verdict: [ACCEPTED]'
-  else:
-    s += 'verdict: [WA: {0}] [TLE: {1}]'.format(WA_CNT, TLE_CNT)
-  if verbose: print(s)
-  if WA_CNT + TLE_CNT == 0:
-    feedback.set_grade(100)
-    feedback.set_global_result("success")
-    feedback.set_global_feedback(s)
-  else:
-    percentage = 100 * (n - WA_CNT - TLE_CNT) / n
-    if verbose: print(percentage)
-    feedback.set_grade(percentage)
-    feedback.set_global_result("failure")
-    feedback.set_global_feedback(s)
-  
-if verbose: print('compiling my solution')
-
-ok, msg = compile_java(solname)
-if not ok:
-  if(verbose): print('compilation error on my solution:')
-  if(verbose): print(msg)
-  os.system('feedback --result failed --feedback "{0}\n{1}"'.format('ups, it seems there is a problem. contact administrator.', message))  
-  exit(0)
-
 os.system('getinput {0} > Main.java'.format(taskname))
-
-ok, msg = compile_java('Main')
-
-if not ok:
-  if(verbose): print('compilation error:')
-  if(verbose): print(msg)
-  os.system('feedback --result failed --feedback "{0}\n{1}"'.format('compilation error', msg))
-  exit(0)
-
-student_times, my_times, verdict, student_feedback = run_tests_java()
-output_feedback(student_times, my_times, verdict, student_feedback)
+judging = judge_java('Main', './tests', checker, timelimit):
+if judging.is_accepted():
+ feedback.set_global_result("success")
+else:
+  feedback.set_global_result("failure")
+feedback.set_global_feedback(judging.produce_feedback_message())
