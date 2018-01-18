ee348d92d645a9e803212832b2cfdd37ad2d5c8f
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-01-18 14:49:07 +0100

    Automatic synchronization from INGInious

--- helloworld/checkers.py
+++ helloworld/checkers.py
@@ -2,8 +2,15 @@ import filecmp
 from difflib import Differ
 
 def diff_check(input, output, given):
-  d = Differ()
-  result = list(d.compare(output, given))
-  for line in result:
-    print(line)
+  f = open(output, 'r')
+  lines1 = [line.strip() for line in f.readlines()]
+  f = open(given, 'r')
+  lines2 = [line.strip() for line in f.readlines()]
+  print('expected:')
+  for i in range(len(lines1)):
+    print(lines1[i])
+  print('-----')
+  print('given:')
+  for i in range(len(lines2)):
+    print(lines2[i])
   return filecmp.cmp(output, given)
