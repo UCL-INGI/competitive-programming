ed2187273881a5ad6a3205c36f5568bd30378120
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-02-22 09:58:00 +0100

    Automatic synchronization from INGInious

--- graphs-maze/run_tools.py
+++ graphs-maze/run_tools.py
@@ -165,7 +165,7 @@ def judge(filename, compile, run, checker, timelimit, testdir = './tests', verbo
           answer_ok, msg = checker(testdir + '/' + fn, testdir + '/' + name + '.ans', 'output.tmp')
         except Exception as e:
           answer_ok = False
-          msg = 'the output you provided does not respected the specifications'
+          msg = 'the output you provided does not respect the specifications'
         if not answer_ok:
           if(verbose): print('wrong answer')
           # wrong_answer
