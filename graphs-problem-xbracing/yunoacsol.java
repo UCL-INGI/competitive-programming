2531dead6d68960d121019b899f02b4acdd9ad68
francois <f.aubry@uclouvain.be>
2018-03-16 05:03:38 +1300

    up

--- graphs-problem-xbracing/yunoacsol.java
+++ graphs-problem-xbracing/yunoacsol.java
@@ -25,96 +25,96 @@ import java.util.Scanner;
 
 
 
- */
+*/
 
 public class yunoacsol {
 
-	public static void main(String[] args) throws IOException {
-		
-		solve(new Scanner(System.in), new PrintWriter(System.out));	
-	}
-	
-	static void genOutputs() throws IOException {
-		File dir = new File("CrossBracing/in/");
-		for(File f : dir.listFiles()) {
-			File out = new File("CrossBracing/out/" + "out" + f.getName().substring(2));
-			solve(new Scanner(new FileReader(f)), new PrintWriter(new FileWriter(out)));
-		}
-	}
-	
-	static void solve(Scanner reader, PrintWriter writer) {
-		r = reader.nextInt();
-		c = reader.nextInt();
-		// rows: 0 to r - 1
-		// columns: r to r + c - 1
-		g = new ArrayList<>();
-		for(int i = 0; i < r + c; i++) {
-			g.add(new LinkedList<Integer>());
-		}
-		for(int i = 0; i < r; i++) {
-			String s = reader.next();
-			for(int j = 0; j < s.length(); j++) {
-				if(s.charAt(j) == '1') {
-					g.get(i).add(r + j);
-					g.get(r + j).add(i);
-				}
-			}
-		}
-		int min = computeMin();
-		writer.println(min);
-		writer.close();
-		reader.close();
-	}
-	
-	static int r, c;
-	static ArrayList<LinkedList<Integer>> g;
-	
-	static int computeMin() {
-		Integer[] component = new Integer[g.size()];
-		for(int x = 0; x < g.size(); x++) {
-			if(component[x] == null) {
-				bfsVisit(x, component);
-			}
-		}
-		HashSet<Integer> H = new HashSet<>();
-		for(int c : component) {
-			H.add(c);
-		}
-		return H.size() - 1;
-	}
-	
-	static void bfsVisit(int s, Integer[] component) {
-		Queue<Integer> Q = new LinkedList<>();
-		Q.add(s);
-		component[s] = s;
-		while(!Q.isEmpty()) {
-			int x = Q.poll();
-			for(int y : g.get(x)) {
-				if(component[y] == null) {
-					Q.add(y);
-					component[y] = s;
-				}
-			}
-		}
-	}
-	
-	// GENERATE INPUT
-	
-	static void genRandomInput(int C, int R, int p) throws IOException {
-		Random rnd = new Random();
-		PrintWriter writer = new PrintWriter(new FileWriter("CrossBracing/in/rnd_"  + C + "_" + R + "_" + p));
-		writer.append(C + " " + R + "\n");
-		for(int i = 0; i < C; i++) {
-			for(int j = 0; j < R; j++) {
-				boolean cross = true;
-				for(int k = 0; k < p; k++) {
-					if(rnd.nextBoolean()) cross = false;
-				}
-				writer.append(cross ? '1' : '0');
-			}
-			writer.println();
-		}
-		writer.close();
-	}
-	
+public static void main(String[] args) throws IOException {
+
+solve(new Scanner(System.in), new PrintWriter(System.out));
+}
+
+static void genOutputs() throws IOException {
+File dir = new File("CrossBracing/in/");
+for(File f : dir.listFiles()) {
+File out = new File("CrossBracing/out/" + "out" + f.getName().substring(2));
+solve(new Scanner(new FileReader(f)), new PrintWriter(new FileWriter(out)));
+}
+}
+
+static void solve(Scanner reader, PrintWriter writer) {
+r = reader.nextInt();
+c = reader.nextInt();
+// rows: 0 to r - 1
+// columns: r to r + c - 1
+g = new ArrayList<>();
+for(int i = 0; i < r + c; i++) {
+g.add(new LinkedList<Integer>());
+}
+for(int i = 0; i < r; i++) {
+String s = reader.next();
+for(int j = 0; j < s.length(); j++) {
+if(s.charAt(j) == '1') {
+g.get(i).add(r + j);
+g.get(r + j).add(i);
+}
+}
+}
+int min = computeMin();
+writer.println(min);
+writer.close();
+reader.close();
+}
+
+static int r, c;
+static ArrayList<LinkedList<Integer>> g;
+
+static int computeMin() {
+Integer[] component = new Integer[g.size()];
+for(int x = 0; x < g.size(); x++) {
+if(component[x] == null) {
+bfsVisit(x, component);
+}
+}
+HashSet<Integer> H = new HashSet<>();
+for(int c : component) {
+H.add(c);
+}
+return H.size() - 1;
+}
+
+static void bfsVisit(int s, Integer[] component) {
+Queue<Integer> Q = new LinkedList<>();
+Q.add(s);
+component[s] = s;
+while(!Q.isEmpty()) {
+int x = Q.poll();
+for(int y : g.get(x)) {
+if(component[y] == null) {
+Q.add(y);
+component[y] = s;
+}
+}
+}
+}
+
+// GENERATE INPUT
+
+static void genRandomInput(int C, int R, int p) throws IOException {
+Random rnd = new Random();
+PrintWriter writer = new PrintWriter(new FileWriter("CrossBracing/in/rnd_"  + C + "_" + R + "_" + p));
+writer.append(C + " " + R + "\n");
+for(int i = 0; i < C; i++) {
+for(int j = 0; j < R; j++) {
+boolean cross = true;
+for(int k = 0; k < p; k++) {
+if(rnd.nextBoolean()) cross = false;
+}
+writer.append(cross ? '1' : '0');
+}
+writer.println();
+}
+writer.close();
+}
+
 }
