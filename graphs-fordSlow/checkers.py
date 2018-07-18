7b2a1c831c9527103b0b10c7daa39cc50c4d6062
francois <f.aubry@uclouvain.be>
2018-07-18 14:17:49 +0200

    up

--- graphs-fordSlow/checkers.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-cmd = ['sleep', '10']
-try:
-  subprocess.run(cmd, timeout=1)
-except subprocess.TimeoutExpired:
-  print('process ran too long')
--- graphs-fordSlow/config.py
+++
@@ -1,7 +0,0 @@
-import checkers
-
-READ = False
-TASKNAME = 'file'
-SOLNAME = 'yunoacsol'
-TIMELIMIT = 2
-CHECKER = checkers.diff_check
